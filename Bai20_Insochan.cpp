36b65d5ded09b1abfc9483e064f7171e73ab8e26
NguyenPhuThanh2982008 <nguyenphuthanhpch298@gmail.com>
2024-11-25 16:41:15 +0700

    Update

--- Bai20_Insochan.cpp
+++ Bai20_Insochan.cpp
@@ -6,18 +6,41 @@ using namespace std;
 int main()
 {
     cout << "Xet so co phai la so chan hay khong\n";
-    int so;
-    cout << "Nhap so muon biet la so chan: "; cin >> so;
+    int so, tongchan = 0, tongle = 0, tong = 0;
+    cout << "Nhap so: "; cin >> so;
+
+    cout << "Cac so chan tu 1 den " << so <<" vua nhap : \n";
+    for (int i = 1; i < so; i++)
+    {
+        tong += i;
+        if (i % 2 == 0)
+        {
+            cout << i << " ";
+            tongchan += i;
+        }
+        else 
+        {
+            tongle += i;
+        }
+    }
+
+    cout << endl;    cout << endl;
+
+    cout << "Cac so le tu 1 den " << so << " vua nhap : \n";
+    for (int i = 0; i < so; i++)
+    {
+        if (i % 2 != 0)
+        {
+            cout << i << " ";
+        }
+    }
 
 
-    if (so % 2 == 0)
-    {
-        cout << "So " << so << " la so chan\n";
-    }
-    else
-    {
-        cout << "So " << so << " la so le\n";
-    }
+    cout << endl;    cout << endl;
+
+    cout << "Tong cac so chan tu 1 den " << so << " la: " << tongchan << endl;
+    cout << "Tong cac so le tu 1 den " << so << " la: " << tongle << endl;
+    cout << "Tong cac so tu 1 den " << so << " la: " << tong << endl;
 
     return 0;
 }
